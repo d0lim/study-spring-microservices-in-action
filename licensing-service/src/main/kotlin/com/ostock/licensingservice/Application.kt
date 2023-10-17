4c7d3d9d9930801e49aac9844a10668408a5222d
d0lim <limdongyoung0@gmail.com>
2023-10-17 21:30:18 +0900

    refactor: application name

--- licensing-service/src/main/kotlin/com/ostock/licensingservice/Application.kt
+++ licensing-service/src/main/kotlin/com/ostock/licensingservice/Application.kt
@@ -4,8 +4,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class LicensingServiceApplication
+class Application
 
 fun main(args: Array<String>) {
-    runApplication<LicensingServiceApplication>(*args)
+    runApplication<Application>(*args)
 }
