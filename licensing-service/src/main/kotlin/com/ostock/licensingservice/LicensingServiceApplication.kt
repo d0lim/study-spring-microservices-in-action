dea750c3f7cd8cef1b86697c770c84f441dd4e70
d0lim <limdongyoung0@gmail.com>
2023-10-17 21:29:04 +0900

    init: repo

---
+++ licensing-service/src/main/kotlin/com/ostock/licensingservice/LicensingServiceApplication.kt
@@ -0,0 +1,11 @@
+package com.ostock.licensingservice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class LicensingServiceApplication
+
+fun main(args: Array<String>) {
+    runApplication<LicensingServiceApplication>(*args)
+}
