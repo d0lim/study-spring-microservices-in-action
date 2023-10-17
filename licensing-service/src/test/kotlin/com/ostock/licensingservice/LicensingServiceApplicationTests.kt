56790adcb0f7970c4634b5aa2fb0e38bb5659c14
d0lim <limdongyoung0@gmail.com>
2023-10-17 21:29:04 +0900

    init: repo

---
+++ licensing-service/src/test/kotlin/com/ostock/licensingservice/LicensingServiceApplicationTests.kt
@@ -0,0 +1,12 @@
+package com.ostock.licensingservice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class LicensingServiceApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+}
