82058c97fa7d044544d51ab046dc112685146279
d0lim <limdongyoung0@gmail.com>
2023-10-23 20:22:28 +0900

    init: config-server repo

---
+++ config-server/src/main/kotlin/com/example/configserver/ConfigServerApplication.kt
@@ -0,0 +1,11 @@
+package com.example.configserver
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ConfigServerApplication
+
+fun main(args: Array<String>) {
+    runApplication<ConfigServerApplication>(*args)
+}
