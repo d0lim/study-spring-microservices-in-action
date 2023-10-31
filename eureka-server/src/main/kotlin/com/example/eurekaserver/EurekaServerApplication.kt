d72ff75a52015c79e4868252795448062edec8be
d0lim <limdongyoung0@gmail.com>
2023-10-31 21:01:17 +0900

    init: eureka-server

---
+++ eureka-server/src/main/kotlin/com/example/eurekaserver/EurekaServerApplication.kt
@@ -0,0 +1,11 @@
+package com.example.eurekaserver
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class EurekaServerApplication
+
+fun main(args: Array<String>) {
+    runApplication<EurekaServerApplication>(*args)
+}
