7beaa9383317052479d4d6ca7c518bea7899beb7
d0lim <limdongyoung0@gmail.com>
2023-10-31 21:07:14 +0900

    feat: add plugin and configure as eureka server

--- eureka-server/build.gradle.kts
+++ eureka-server/build.gradle.kts
@@ -3,6 +3,8 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     id("org.springframework.boot") version "3.1.5"
     id("io.spring.dependency-management") version "1.1.3"
+    id("org.jlleitschuh.gradle.ktlint") version "11.6.1"
+    id("com.google.cloud.tools.jib") version "3.4.0"
     kotlin("jvm") version "1.8.22"
     kotlin("plugin.spring") version "1.8.22"
 }
@@ -44,4 +46,23 @@ tasks.withType<KotlinCompile> {
 
 tasks.withType<Test> {
     useJUnitPlatform()
+}
+
+jib {
+    from {
+        platforms {
+            platform {
+                architecture = "arm64"
+                os = "linux"
+            }
+//            platform {
+//                architecture = "amd64"
+//                os = "linux"
+//            }
+        }
+    }
+    to {
+        image = "ostock/licensing-service"
+        tags = setOf("latest")
+    }
 }
