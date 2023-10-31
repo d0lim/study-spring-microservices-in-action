434be6e13176d37f481f21387b01c26e48fb002d
d0lim <limdongyoung0@gmail.com>
2023-10-31 21:51:45 +0900

    refactor: add new services to docker and scripts

--- scripts/build-and-run.sh
+++ scripts/build-and-run.sh
@@ -1,7 +1,13 @@
 cd config-server
 ./gradlew jibDockerBuild
 cd ..
+cd eureka-server
+./gradlew jibDockerBuild
+cd ..
 cd licensing-service
+./gradlew jibDockerBuild
+cd ..
+cd organization-service
 ./gradlew jibDockerBuild
 cd ..
 docker compose -f ./docker/docker-compose.yml up -d
